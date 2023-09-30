b94754a8d520132c35eb29880936e543ff1c515f
BableshAAzad <bableshaazad@outlook.com>
2023-09-30 23:47:15 +0530

    stack implementation by using single linkedLink

--- CollectionsClasses/StackImplimentationSingleLinkedList/MyStackSingleLinkedList.java
+++ CollectionsClasses/StackImplimentationSingleLinkedList/MyStackSingleLinkedList.java
@@ -9,7 +9,8 @@ public class MyStackSingleLinkedList {
             this.data = data;
         }
     }
-// --------------------------------------------
+
+    // --------------------------------------------
     Node top;
     int count = 0;
 
@@ -19,13 +20,41 @@ public class MyStackSingleLinkedList {
         top = n;
         count++;
     }
+
     // -----------------------------------------
-    public Object peek(){
-      if(top== null)
-        return "Stack is underflow";
-      else 
-        return top.data;
+    public Object peek() {
+        if (top == null)
+            return "Stack is underflow";
+        else
+            return top.data;
     }
+
     // ----------------------------------------
-    
+    public Object pop() {
+        if (top == null)
+            return "Stack is underflow";
+        Object data = top.data;
+        top = top.next;
+        count--;
+        return data;
+    }
+
+    // -----------------------------------------
+    @Override
+    public String toString() {
+        String str = "";
+        Node temp = top;
+        while (temp != null) {
+            str = str + temp.data;
+            if (temp.next != null)
+                str = str + "<=";
+            temp = temp.next;
+        }
+        return str;
+    }
+
+    // ------------------------------------------
+    public int size() {
+        return count;
+    }
 }
