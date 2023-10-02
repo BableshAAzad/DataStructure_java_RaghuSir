962c26048fb91fb5268c9b5e13889e5576831ba1
BableshAAzad <bableshaazad@outlook.com>
2023-10-02 22:50:12 +0530

    String to int convert using Integer.parseInt(arg)

--- Practice.java
+++ Practice.java
@@ -1,5 +1,9 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println(args.length);
+        String a = "12";
+        int c = Integer.parseInt(a);
+        int b = 12;
+        System.out.println(c==b);
     }
 }
