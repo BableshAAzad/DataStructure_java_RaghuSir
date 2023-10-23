133e6939d632b479a4434c196c988031a5a0d350
BableshAAzad <bableshaazad@outlook.com>
2023-10-23 23:04:34 +0530

    File copied one to another

---
+++ FileHandlingIOPakage/ReadMultipleFileAndConvertSingleFile/ReadMuxFileAndConVertSingle.java
@@ -0,0 +1,23 @@
+package FileHandlingIOPakage.ReadMultipleFileAndConvertSingleFile;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ReadMuxFileAndConVertSingle {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fin = new FileInputStream(
+                "D:\\a. programming\\java\\BableshWorkSpace\\DataStructure_java_RaghuSir\\FileHandlingIOPakage\\AAzadData\\RawData\\BableshDetails.txt");
+        FileOutputStream fout = new FileOutputStream(
+                "D:\\a. programming\\java\\BableshWorkSpace\\DataStructure_java_RaghuSir\\FileHandlingIOPakage\\AAzadData\\WriteData\\DetailsDublicate.txt");
+        while (true) {
+            int x = fin.read();
+            if (x == -1)
+                break;
+            fout.write(x);
+        }
+        fout.flush();
+        fout.close();
+        fin.close();
+        System.out.println("Data copied successfully done");
+    }
+}
