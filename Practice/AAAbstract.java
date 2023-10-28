4d7da1e64cca7844128232daa2549f0378788411
BableshAAzad <bableshaazad@outlook.com>
2023-10-28 14:47:36 +0530

    interface java

---
+++ Practice/AAAbstract.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public abstract class AAAbstract extends AAClass implements AAInterface {
+    // @Override
+    // public void mi(){
+    //     System.out.println("hi");
+    // }
+}
